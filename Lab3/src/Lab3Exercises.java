83e599af552b19603e7f0324e0e8f183d98a3249
4015a17 <4015a17@anon.ece.uprm.edu>
2017-11-15 13:22:00 -0400

    codes

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,9 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Julio Torres");
 		myFrame .setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         myFrame .setLocation(850,400);
-		myFrame .setSize(200, 200);
+		myFrame .setSize(300, 300);
 		myFrame .setVisible(true);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 	}
 }
